9f1251b090d4c884eb5b15b7f66212d9a34c81dd
Sushant Neupane <nsushant09@gmail.com>
2023-10-31 11:14:43 +0545

    Changes

--- FreemanBackend/src/main/java/com/example/FreemanBackend/core/JWTService.java
+++
@@ -1,7 +0,0 @@
-package com.example.FreemanBackend.core;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class JWTService {
-}
--- FreemanBackend/src/main/java/com/example/FreemanBackend/model/User.java
+++ FreemanBackend/src/main/java/com/example/FreemanBackend/model/User.java
@@ -2,10 +2,14 @@ package com.example.FreemanBackend.model;
 
 import com.example.FreemanBackend.core.Constants;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Table(name = Constants.USER_TABLE)
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer id;
@@ -22,5 +26,40 @@ public class User {
         this.dateOfBirth = null;
         this.gender = null;
         this.role = null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
